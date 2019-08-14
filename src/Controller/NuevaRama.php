f76115c7de65707036e36c12f13b810754ab8ca8
Nolvis Perez <nolvisfranco@gmail.com>
2019-08-14 13:54:00 +0200

    subiendo NuevaRama.php al servidor remoto

--- src/Controller/NuevaRama.php
+++ src/Controller/NuevaRama.php
@@ -4,7 +4,13 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+
 class NuevaRama
 {
+    public function index($name)
+    {
+        return new Response('Hello $name!');
+    }
 
 }
